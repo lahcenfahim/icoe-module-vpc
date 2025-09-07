4b0bc2298ad0705ad79113ac61121d668a302e65
EL-FAHIM  Lahcen <fahim.etu@gmail.com>
2025-09-07 09:27:43 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "name" {
+  description = "Name tag for the VPC"
+  type        = string
+  default     = "default-vpc"
+}
